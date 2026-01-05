b4b91749b450d3ab0b3f44f5371dd7a66e421368
manasa <manasac-cs@dayanandasagar.edu>
2026-01-05 21:05:36 +0530

    initial maven project

---
+++ src/test/java/first/ResultTest.java
@@ -0,0 +1,24 @@
+package first;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ResultTest {
+
+    @Test
+    public void testPass() {
+        Assert.assertEquals(Result.display(55), "pass");
+    }
+
+    @Test
+    public void testFail() {
+        Assert.assertEquals(Result.display(35), "fail");
+    }
+
+    @Test
+    public void testInvalid() {
+        Assert.assertEquals(Result.display(-5), "invalid");
+    }
+}
+
+
